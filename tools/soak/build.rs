e2138ed6640e8d56e275e7cc2990d9dcfee5eb37
Duocast <65754673+Duocast@users.noreply.github.com>
2026-02-17 16:47:17 -0700

    Create build.rs

---
+++ tools/soak/build.rs
@@ -0,0 +1,30 @@
+use std::{env, path::PathBuf};
+
+fn main() {
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let proto_dir = env::var("PROTO_DIR")
+        .map(PathBuf::from)
+        .unwrap_or_else(|_| manifest_dir.join("../../proto"));
+
+    let protos = [
+        "common.proto",
+        "caps.proto",
+        "auth.proto",
+        "channel.proto",
+        "presence.proto",
+        "chat.proto",
+        "moderation.proto",
+        "telemetry.proto",
+        "control.proto",
+    ];
+
+    let proto_paths: Vec<PathBuf> = protos.iter().map(|p| proto_dir.join(p)).collect();
+
+    for p in &proto_paths {
+        println!("cargo:rerun-if-changed={}", p.display());
+    }
+
+    prost_build::Config::new()
+        .compile_protos(&proto_paths, &[proto_dir])
+        .unwrap();
+}
