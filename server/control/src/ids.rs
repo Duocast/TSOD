a86d8dc939ed585045b248700951ce68898627cc
Duocast <65754673+Duocast@users.noreply.github.com>
2026-02-17 15:30:15 -0700

    Update ids.rs

--- server/control/src/ids.rs
+++ server/control/src/ids.rs
@@ -1,24 +1,21 @@
-use std::fmt;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct UserId(pub String);
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct ServerId(pub Uuid);
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct ServerId(pub String);
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct UserId(pub Uuid);
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct ChannelId(pub String);
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct ChannelId(pub Uuid);
 
-impl fmt::Display for UserId {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { self.0.fmt(f) }
+impl ServerId {
+    pub fn new() -> Self { Self(Uuid::new_v4()) }
 }
-impl fmt::Display for ServerId {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { self.0.fmt(f) }
+impl UserId {
+    pub fn new() -> Self { Self(Uuid::new_v4()) }
 }
-impl fmt::Display for ChannelId {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { self.0.fmt(f) }
-}
-
 impl ChannelId {
-    pub fn new() -> Self { Self(uuid::Uuid::new_v4().to_string()) }
+    pub fn new() -> Self { Self(Uuid::new_v4()) }
 }
