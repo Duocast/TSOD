5c3bbf6ae3f0c73fb07140d1b586b39f376a0500
Duocast <65754673+Duocast@users.noreply.github.com>
2026-02-20 13:41:48 -0700

    Update mod.rs

--- server/gateway/src/proto/mod.rs
+++ server/gateway/src/proto/mod.rs
@@ -1,3 +1,5 @@
-// Prost will generate nested modules for package voiceplatform.v1
-// into a file named "voiceplatform.v1.rs".
-include!(concat!(env!("OUT_DIR"), "/voiceplatform.v1.rs"));
+pub mod voiceplatform {
+    pub mod v1 {
+        include!(concat!(env!("OUT_DIR"), "/voiceplatform.v1.rs"));
+    }
+}
