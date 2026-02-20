77df7d9ab6e5a45a73f7f018e995e00b7cd05492
Duocast <65754673+Duocast@users.noreply.github.com>
2026-02-20 11:57:31 -0700

    Update ids.rs

--- server/control/src/ids.rs
+++ server/control/src/ids.rs
@@ -13,16 +13,40 @@ pub struct ChannelId(pub Uuid);
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct MessageId(pub Uuid);
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct OutboxId(pub Uuid);
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct AuditId(pub Uuid);
+
 impl ServerId {
-    pub fn new() -> Self { Self(Uuid::new_v4()) }
+    pub fn new() -> Self {
+        Self(Uuid::new_v4())
+    }
 }
 impl UserId {
-    pub fn new() -> Self { Self(Uuid::new_v4()) }
+    pub fn new() -> Self {
+        Self(Uuid::new_v4())
+    }
 }
 impl ChannelId {
     pub fn new() -> Self { Self(Uuid::new_v4()) }
 }
 
 impl MessageId {
-    pub fn new() -> Self { Self(Uuid::new_v4()) }
+    pub fn new() -> Self {
+        Self(Uuid::new_v4())
+    }
+}
+
+impl OutboxId {
+    pub fn new() -> Self {
+        Self(Uuid::new_v4())
+    }
+}
+
+impl AuditId {
+    pub fn new() -> Self {
+        Self(Uuid::new_v4())
+    }
 }
