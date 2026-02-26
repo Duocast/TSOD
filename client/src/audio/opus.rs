9fd16773d3a68cc1761b1da6193a6018c6b0b483
Duocast <65754673+Duocast@users.noreply.github.com>
2026-02-26 06:32:17 -0700

    Fix multi-user receive audio stream isolation

--- client/src/audio/opus.rs
+++ client/src/audio/opus.rs
@@ -1,20 +1,38 @@
 use anyhow::Result;
 
-pub struct OpusCodec {
+pub struct OpusEncoder {
     enc: opus::Encoder,
+}
+
+pub struct OpusDecoder {
     dec: opus::Decoder,
 }
 
-impl OpusCodec {
+impl OpusEncoder {
     pub fn new(sample_rate: u32, channels: u8) -> Result<Self> {
-        let ch = if channels == 2 { opus::Channels::Stereo } else { opus::Channels::Mono };
+        let ch = if channels == 2 {
+            opus::Channels::Stereo
+        } else {
+            opus::Channels::Mono
+        };
         let enc = opus::Encoder::new(sample_rate, ch, opus::Application::Voip)?;
-        let dec = opus::Decoder::new(sample_rate, ch)?;
-        Ok(Self { enc, dec })
+        Ok(Self { enc })
     }
 
     pub fn encode(&mut self, pcm: &[i16], out: &mut [u8]) -> Result<usize> {
         Ok(self.enc.encode(pcm, out)?)
+    }
+}
+
+impl OpusDecoder {
+    pub fn new(sample_rate: u32, channels: u8) -> Result<Self> {
+        let ch = if channels == 2 {
+            opus::Channels::Stereo
+        } else {
+            opus::Channels::Mono
+        };
+        let dec = opus::Decoder::new(sample_rate, ch)?;
+        Ok(Self { dec })
     }
 
     pub fn decode(&mut self, data: &[u8], pcm_out: &mut [i16]) -> Result<usize> {
